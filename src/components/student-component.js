2f97e6b97eca12ef98314d722e2560314df08879
Liu <jerryliu@gmail.com>
2024-06-03 01:23:05 +0800

    將學生資料以table方式呈現並設定CSS屬性

--- src/components/student-component.js
+++ src/components/student-component.js
@@ -1,13 +1,25 @@
 import { useLocation } from "react-router-dom";
+import "../style/style.css";
 
 const StudentComponent = () => {
   const location = useLocation();
   const { data } = location.state || {}; // 解構state對象的data屬性
   let newData = data.slice(0, -1);
+  let result = newData.split(",");
   return (
     <div>
+      {" "}
       <h1>Student Data</h1>
-      <p>{newData}</p>
+      <table>
+        <tr>
+          <th>學生姓名</th>
+        </tr>
+        {newData.split(",").map((char) => (
+          <tr>
+            <td>{char}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
